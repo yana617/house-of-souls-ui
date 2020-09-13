62c47763cf2f774213ff2dd583f09e91f6cac1e0
Yana <jana.sidorova@celadon.ae>
2020-09-13 14:09:35 +0300

    fix-deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     production: {
       user: 'root',
       host: '167.71.63.10',
-      ref: 'origin/master',
+      ref: 'origin/fix-deploy',
       repo: 'git@github.com:yana617/house-of-souls-ui.git',
       path: '/var/www/house-of-souls-ui',
       'pre-deploy-local': '',
