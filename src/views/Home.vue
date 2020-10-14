a4a043a440c2c8c80cb35a35cdf13fcb54675fde
Yana Sidorova <yanasidorova@MacBook-Pro-Yana.local>
2020-10-14 14:08:31 +0300

    footer component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,31 @@
   <div class="home">
     <span id="title">Главная страница</span>
   </div>
+  <Footer />
 </template>
 
 <script>
+import Footer from '../components/Footer.vue';
+
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    Footer,
+  },
 };
 </script>
+
+<style scoped lang="scss">
+$headerHeight: 50px;
+$footerHeight: 60px;
+$footerHeightMobile: 90px;
+
+.home {
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - #{$headerHeight + $footerHeight});
+  @media (max-width: 500px) {
+    min-height: calc(100vh - #{$headerHeight + $footerHeightMobile});
+  }
+}
+</style>
