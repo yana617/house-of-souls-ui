76417c3dc545876fe1564b4c05754547ddeb73ed
yana617 <31696608+yana617@users.noreply.github.com>
2023-05-22 05:08:29 +0700

    Update node to v18 (#50)
    * Update node to v18
    
    * Update github actions on pull request

--- src/mocks/userAdditionalFields/index.js
+++ src/mocks/userAdditionalFields/index.js
@@ -4,7 +4,7 @@ import { rest } from 'msw';
 import { API_HOST } from '@/constants';
 import { IS_AUTH } from '@/mocks/constants';
 
-import data from './mocks.json';
+import userAdditionalFieldMocks from './mocks';
 
 export default [
   rest.get(`${API_HOST}/user-additional-fields`, (req, res, ctx) => {
@@ -18,7 +18,7 @@ export default [
     }
 
     const userId = req.url.searchParams.get('userId');
-    const responseData = data.userAdditionalFields.filter((uaf) => uaf.user_id === userId);
+    const responseData = userAdditionalFieldMocks.filter((uaf) => uaf.user_id === userId);
 
     return res(
       ctx.status(200),
@@ -41,13 +41,13 @@ export default [
     const { id } = req.params;
     const value = req.url.searchParams.get('value');
 
-    const userAdditionalFieldFromDB = data.userAdditionalFields.find((uaf) => uaf._id === id);
+    const userAdditionalFieldFromDB = userAdditionalFieldMocks.find((uaf) => uaf._id === id);
 
     if (!userAdditionalFieldFromDB) return res(ctx.status(404));
 
-    data.userAdditionalFields.forEach((uaf, index) => {
+    userAdditionalFieldMocks.forEach((uaf, index) => {
       if (uaf._id === id) {
-        data.userAdditionalFields[index] = {
+        userAdditionalFieldMocks[index] = {
           ...userAdditionalFieldFromDB,
           value: value === 'true',
         };
