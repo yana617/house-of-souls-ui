7b4d7102cf9eebf18072e1ce1e2e0598f07c80a0
yana617 <31696608+yana617@users.noreply.github.com>
2026-04-02 19:20:41 +0300

    Release/3.0.0 (#157)
    * feat: up varsions
    
    * fix: after BE updates

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,7 @@
 export const findError = (errors, field) => {
-  if (!errors || !errors.some((err) => err.param === field)) {
+  if (!errors || !errors.some((err) => err.path === field)) {
     return null;
   }
-  const error = errors.find((err) => err.param === field);
+  const error = errors.find((err) => err.path === field);
   return error.msg;
 };
